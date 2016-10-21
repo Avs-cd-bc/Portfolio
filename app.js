1e1a78c7a6885ef6b1bc9ae6537e71c46bed47d3
AvsCode <AvsCode@gmail.com>
2016-10-21 15:00:38 -0700

    updating app.js<F11

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+
 var express = require("express");
+var path = require("path");
 
 const port = 8081;
 const app = express();
 
+app.use(express.static("views"));
+
 app.get("/", function(req,res){
-  res.send("Hello World");
+  res.sendFile(path.join(__dirname, "./views/index.html"));
 });
 
 app.listen(port, function(){
