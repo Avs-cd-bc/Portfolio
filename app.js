b907438d1423baccfd875bdc51e966122356816f
AvsCode <AvsCode@gmail.com>
2016-11-03 15:37:15 -0700

    Updating to react app

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 
-var express = require("express");
-var path = require("path");
 
-const port = 8081;
-const app = express();
-
-app.use(express.static("views"));
-
-app.get("/", function(req,res){
-  res.sendFile(path.join(__dirname, "./views/index.html"));
-});
-
-app.get("/images/ithub", function(req, res){
-  res.sendFile(path.join(__dirname, "./images/GitHub_Logo.png"));
-});
-
-app.get("/images/github-mark", function(req, res){
-  res.sendFile(path.join(__dirname, "./images/GitHub-Mark-32px.png"));
-});
-
-app.get("/images/github", function(req, res){
-  res.sendFile(path.join(__dirname, "./images/GitHub_Logo.png"));
-});
-
-app.listen(port, function(){
-  console.log("Hello world is running!");
-});
+//
+// var express = require("express");
+// var path = require("path");
+//
+// const port = 8081;
+// const app = express();
+//
+// app.use(express.static("views"));
+//
+// app.get("/", function(req,res){
+//   res.sendFile(path.join(__dirname, "./views/index.html"));
+// });
+//
+// app.get("/images/ithub", function(req, res){
+//   res.sendFile(path.join(__dirname, "./images/GitHub_Logo.png"));
+// });
+//
+// app.get("/images/github-mark", function(req, res){
+//   res.sendFile(path.join(__dirname, "./images/GitHub-Mark-32px.png"));
+// });
+//
+// app.get("/images/github", function(req, res){
+//   res.sendFile(path.join(__dirname, "./images/GitHub_Logo.png"));
+// });
+//
+// app.get("/images/background", function(req, res){
+//   res.sendFile(path.join(__dirname, "./images/background.jpg"))
+// });
+//
+// app.get("/styles/styles", function(req,res){
+//   res.sendFile(path.join(__dirname,"./styles/styles.css"))
+// });
+//
+// app.listen(port, function(){
+//   console.log("Hello world is running!");
+// });
